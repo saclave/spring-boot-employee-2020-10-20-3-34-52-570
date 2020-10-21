cdd86e0e5afc85f620037b6e334ed41eac01da34
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-21 19:43:03 +0800

    [REST][Vea&Joseph] test - should_return_list_of_companies_when_get_all_companies_given_get_request

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CompanyServiceTest {
+
+    private CompanyRepository companyRepository;
+    private CompanyService companyService;
+
+    @BeforeEach
+    void setUp() {
+        companyRepository = mock(CompanyRepository.class);
+        companyService = new CompanyService(companyRepository);
+
+    }
+
+    @Test
+    void should_return_list_of_employees_when_get_all_employees_given_get_request() {
+        //given
+
+        //when
+        when(companyRepository.findAllCompanies()).thenReturn(Collections.singletonList(new Company()));
+        List<Employee> actual = companyService.getAllCompanies();
+
+        //then
+        assertEquals(1, actual.size());
+    }
+}
