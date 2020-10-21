1c81bd6d5540b03aca302ac58981c69b33a1002d
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-21 19:12:41 +0800

    [REST][Vea&Joseph] feature - added company controller

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,15 +1,62 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/companies")
 public class CompanyController {
-    private List<Company> Company = new ArrayList<>();
+    private List<Company> companyList = new ArrayList<>();
+    private List<Employee> employeeList = new ArrayList<>();
 
+    @GetMapping
+    public List<Company> getCompanyList() {
+        return companyList;
+    }
+
+    @PostMapping
+    public Company addCompany(@RequestBody Company company) {
+        companyList.add(company);
+        return company;
+    }
+
+    @GetMapping("/{companyId}")
+    public List<Employee> getEmployeeList(@PathVariable int companyId) {
+        return employeeList.stream()
+                .filter(employee -> employee.getCompanyId().equals(companyId))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{companyId}")
+    public Company getCompany(@PathVariable int companyId) {
+        return companyList.stream()
+                .filter(company -> company.getCompanyId().equals(companyId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PutMapping("/{companyId}")
+    public Company updateCompany(@PathVariable Integer companyId, @RequestBody Company companyUpdate) {
+        companyList.stream()
+                .filter(company -> company.getCompanyId().equals(companyId))
+                .findFirst()
+                .ifPresent(company -> {
+                    companyList.remove(company);
+                    companyList.add(companyUpdate);
+                });
+        return companyUpdate;
+    }
+
+    @DeleteMapping("/{companyId}")
+    public void deleteCompany(@PathVariable Integer companyId) {
+        companyList.stream()
+                .filter(company -> company.getCompanyId().equals(companyId))
+                .findFirst()
+                .ifPresent(companyList::remove);
+    }
 }
--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -5,6 +5,32 @@ import java.util.List;
 public class Company {
     private String companyName;
     private int numOfEmployees;
+    private Integer companyId;
+    private List<Employee> employeeList;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getNumOfEmployees() {
+        return numOfEmployees;
+    }
+
+    public void setNumOfEmployees(int numOfEmployees) {
+        this.numOfEmployees = numOfEmployees;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
 
     public List<Employee> getEmployeeList() {
         return employeeList;
@@ -13,6 +39,4 @@ public class Company {
     public void setEmployeeList(List<Employee> employeeList) {
         this.employeeList = employeeList;
     }
-
-    private List<Employee> employeeList;
 }
