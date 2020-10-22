f5b4dd6751c6fb151930f53895ab278db9f83b71
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-22 17:00:53 +0800

    [Baron&Vea] feature - support employeeservice

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -3,13 +3,14 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepositoryLegacy;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private EmployeeRepositoryLegacy employeeRepositoryLegacy;
     private EmployeeRepository employeeRepository;
 
     public EmployeeService(EmployeeRepository employeeRepository) {
@@ -25,20 +26,8 @@ public class EmployeeService {
     }
 
     public Employee update(Integer employeeId, Employee employeeUpdate) {
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
-        if (employeeUpdate.getName() != null) {
-            employee.setName(employeeUpdate.getName());
-        }
-        if (employeeUpdate.getAge() != null) {
-            employee.setAge(employeeUpdate.getAge());
-        }
-        if (employeeUpdate.getGender() != null) {
-            employee.setGender(employeeUpdate.getGender());
-        }
-        if (employeeUpdate.getSalary() != null) {
-            employee.setSalary(employeeUpdate.getSalary());
-        }
-        return employee;
+        employeeUpdate.setId(employeeId);
+        return employeeRepository.save(employeeUpdate);
     }
 
     public Employee getEmployee(Integer employeeId) {
@@ -50,14 +39,15 @@ public class EmployeeService {
     }
 
     public List<Employee> getEmployeesByGender(String gender) {
-        return employeeRepositoryLegacy.findEmployeesByGender(gender);
+        return employeeRepository.findByGender(gender);
     }
 
-    public List<Employee> getPaginatedEmployee(Long page, Long pageSize) {
-        return employeeRepositoryLegacy.findEmployeesByPagination(page, pageSize);
+    public List<Employee> getPaginatedEmployee(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return employeeRepository.findAll(pageable).toList();
     }
 
-    public List<Employee> getEmployeesByCompanyId(Integer companyId) {
-        return employeeRepositoryLegacy.findEmployeesByCompanyId(companyId);
-    }
+//    public List<Employee> getEmployeesByCompanyId(Integer companyId) {
+//        return employeeRepositoryLegacy.findEmployeesByCompanyId(companyId);
+//    }
 }
