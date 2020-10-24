ec6e1bac39997c6b5c5e3cd0e3efed918cf11329
LAVADPR <lavadpr@oocl.com>
2020-10-24 15:19:04 +0800

    [Prince&Vea] Feature - added two migration sql files

---
+++ src/main/resources/db/migration/V20201024145654__init_db.sql
@@ -0,0 +1,14 @@
+create table company (
+     id bigint not null primary key auto_increment,
+     name varchar(30) not null
+);
+
+create table employee (
+     id bigint not null primary key auto_increment,
+     name varchar(30) not null,
+     age bigint,
+     gender varchar(10),
+     salary bigint,
+     company_id bigint,
+     foreign key (company_id) references company (id)
+);
---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,12 @@
+delete from employee;
+delete from company;
+INSERT INTO company(name) VALUES ('00CL');
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Bob Smith', 10, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Tom Green', 20, 'Female', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Will Smith', 30, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Tom Green', 20, 'Female', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Will Smith', 30, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Billy Butcher', 40, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Star Light', 50, 'Female', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('The Homelander', 60, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('John Snow', 70, 'Male', 1000, 1);
